5c54f7fdad8f776f9a9375fb215f712b82770c49
Salvatore Gionfriddo <salgionfriddo@gmail.com>
2012-04-17 16:18:46 -0400

    Initial Commit

---
+++ Spriter/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Spriter
+//
+//  Created by Salvatore Gionfriddo on 4/17/12.
+//  Copyright __MyCompanyName__ 2012. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+    [pool release];
+    return retVal;
+}
